8c96a86bfa25ad4b158d1d927bc2727077e3b5d0
Kacper Szafrański <kacper.szafranski@pjwstk.edu.pl>
2024-02-12 23:50:35 +0100

    Clean and improve format_converted_csv function

--- convert_pdf_to_csv.py
+++ convert_pdf_to_csv.py
@@ -42,10 +42,10 @@ def join_split_line(i, split_line, columns, condition, is_until_has):
         split_line (list): List of split line elements.
         columns (list): List of columns.
         condition (str): Condition to check.
+        :param is_until_has:
 
     Returns:
         int: Updated index.
-        :param is_until_has:
     """
     # if is_until_has is True, then join until the condition is not found
     if is_until_has:
@@ -75,144 +75,127 @@ def join_split_line(i, split_line, columns, condition, is_until_has):
 # converted csv formatting
 def format_converted_csv(csv_file):
     with open(csv_file, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
+        formatted_lines = []
+        date_pattern = r'\d{8} - \d{8}'
 
-    # removes lowercase letters from each line
-    formatted_lines = ["".join(char for char in line if not char.islower()) for line in lines]
+        for line in file.readlines():
+            # removes lowercase letters from each line
+            formatted_line = "".join(char for char in line if not char.islower())
 
-    # removes periods (.) from each line
-    formatted_lines = [line.replace(".", "") for line in formatted_lines]
+            # removes periods (.) from each line
+            formatted_line = formatted_line.replace(".", "")
 
-    # removes quotes ("") from each line
-    formatted_lines = [line.replace('"', "") for line in formatted_lines]
+            # removes quotes ("") from each line
+            formatted_line = formatted_line.replace('"', "")
 
-    # Pattern to match dates in the format "08022024 - 09032024"
-    pattern = r'\d{8} - \d{8}'
-    formatted_lines = [re.sub(pattern, '', line) for line in formatted_lines]
+            # Pattern to match dates in the format "08022024 - 09032024"
+            formatted_line = re.sub(date_pattern, '', formatted_line)
 
-    # removes last 2 lines
-    formatted_lines = formatted_lines[:-2]
+            formatted_lines.append(formatted_line)
 
-    # if line ens in "PERON" then add a space after it and add the next line to it
-    for i in range(len(formatted_lines)):
-        if formatted_lines[i].endswith("PERON \n"):
-            # adds space before "WARSZAWA"
-            formatted_lines[i] = re.sub(r'WARSZAWA', r' WARSZAWA', formatted_lines[i])
+        # removes last two lines
+        formatted_lines = formatted_lines[:-2]
 
-            # splices the next line to the current line
-            formatted_lines[i] = formatted_lines[i].strip('\n') + ' ' + formatted_lines[i + 1]
+        for i in range(len(formatted_lines)):
+            if formatted_lines[i].endswith("PERON \n"):
+                # adds space before "WARSZAWA"
+                formatted_lines[i] = re.sub(r'WARSZAWA', r' WARSZAWA', formatted_lines[i])
 
-            # adds space after first "9" after "PERON" in formatted_lines[i]
-            formatted_lines[i] = re.sub(r'PERON {2}9', r'PERON 9 ', formatted_lines[i])
+                # splices the next line to the current line
+                formatted_lines[i] = formatted_lines[i].strip('\n') + ' ' + formatted_lines[i + 1]
 
-            formatted_lines[i + 1] = ""
+                # adds space after first "9" after "PERON" in formatted_lines[i]
+                formatted_lines[i] = re.sub(r'PERON {2}9', r'PERON 9 ', formatted_lines[i])
 
-    for i in range(len(formatted_lines)):
-        if formatted_lines[i].endswith("LOTNISKO \n"):
-            # adds space before "WARSZAWA"
-            formatted_lines[i] = re.sub(r'WARSZAWA', r' WARSZAWA', formatted_lines[i])
+                formatted_lines[i + 1] = ""
 
-            # splices the next line to the current line
-            formatted_lines[i] = formatted_lines[i].strip('\n') + ' ' + formatted_lines[i + 1]
+        for i in range(len(formatted_lines)):
+            if formatted_lines[i].endswith("LOTNISKO \n"):
+                # adds space before "WARSZAWA"
+                formatted_lines[i] = re.sub(r'WARSZAWA', r' WARSZAWA', formatted_lines[i])
 
-            # adds space after first "9" after "PERON" in formatted_lines[i]
-            formatted_lines[i] = re.sub(r'LOTNISKO {2}CHOPINA', r'LOTNISKO CHOPINA ', formatted_lines[i])
+                # splices the next line to the current line
+                formatted_lines[i] = formatted_lines[i].strip('\n') + ' ' + formatted_lines[i + 1]
 
-            formatted_lines[i + 1] = ""
+                # adds space after first "9" after "PERON" in formatted_lines[i]
+                formatted_lines[i] = re.sub(r'LOTNISKO {2}CHOPINA', r'LOTNISKO CHOPINA ', formatted_lines[i])
 
-    # small letters fixes
-    # replaces all "EN57ALKM" to "EN57ALwKM"
-    formatted_lines = [re.sub(r'EN57ALKM', r'EN57ALwKM', line) for line in formatted_lines]
+                formatted_lines[i + 1] = ""
 
-    # replaces all "EN57AKM1" to "EN57AKMw1"
-    formatted_lines = [re.sub(r'EN57AKM1', r'EN57AKMw1', line) for line in formatted_lines]
+        pattern_mapping = {
+            r'EN57ALKM': r'EN57ALwKM',
+            r'EN57AKM1': r'EN57AKMw1',
+            r'45WE': r'45WEkm',
+            r'111E': r'111Eb',
+            r'\bB\b': 'Bs',
+            r'\bP\b': 'Ps'
+        }
+        formatted_lines = [re.sub(pattern, replacement, line) for line in formatted_lines for pattern, replacement in pattern_mapping.items()]
 
-    # replaces all "45WE" to "45WEkm"
-    formatted_lines = [re.sub(r'45WE', r'45WEkm', line) for line in formatted_lines]
+        formatted_lines = [line for line in formatted_lines if line.strip()]
 
-    # replaces all "111E" to "111Eb"
-    formatted_lines = [re.sub(r'111E', r'111Eb', line) for line in formatted_lines]
+        for line in formatted_lines:
+            columns = []
+            split_line = line.split()
 
-    # replaces SECOND standalone "B" with "Bs"
-    formatted_lines = [replace_second_occurrence(r'\bB\b', 'Bs', line) for line in formatted_lines]
+            i = 0
+            temp = ''
 
-    # replaces SECOND standalone "P" with "Ps"
-    formatted_lines = [replace_second_occurrence(r'\bP\b', 'Ps', line) for line in formatted_lines]
-
-    # removes empty lines
-    formatted_lines = [line for line in formatted_lines if line.strip()]
-
-    for line in formatted_lines:
-        # Split the line into columns
-        columns = []
-        split_line = line.split(' ')
-        i = 0
-        temp = ''
-
-        # add first column
-        columns.append(split_line[i])
-        i += 1
-
-        # add second column
-        for j in range(i, len(split_line)):
-            if not split_line[j].startswith(('0', '1', '2')):
-                temp += split_line[j] + ' '
-                i += 1
-            else:
-                columns.append(temp.strip())
-                temp = ''
-                break
-
-        # add third column
-        columns.append(split_line[i])
-        i += 1
-
-        # add fourth column
-        for j in range(i, len(split_line)):
-            if not split_line[j].startswith(('0', '1', '2')):
-                temp += split_line[j] + ' '
-                i += 1
-            else:
-                columns.append(temp.strip())
-                temp = ''
-                break
-
-        # add fifth column
-        columns.append(split_line[i])
-        i += 1
-
-        # add sixth column
-        if ',' in split_line[i]:
-            i = join_split_line(i, split_line, columns, ',', True)
-        else:
             columns.append(split_line[i])
             i += 1
 
-        # add seventh column
-        if ',' in split_line[i]:
-            i = join_split_line(i, split_line, columns, ',', True)
-        else:
+            for j in range(i, len(split_line)):
+                if not split_line[j].startswith(('0', '1', '2')):
+                    temp += split_line[j] + ' '
+                    i += 1
+                else:
+                    columns.append(temp.strip())
+                    temp = ''
+                    break
+
             columns.append(split_line[i])
             i += 1
 
-        # add seventh column
-        temp = ''
-        for j in range(i, len(split_line)):
-            temp += split_line[j] + ' '
-        columns.append(temp.strip())
+            for j in range(i, len(split_line)):
+                if not split_line[j].startswith(('0', '1', '2')):
+                    temp += split_line[j] + ' '
+                    i += 1
+                else:
+                    columns.append(temp.strip())
+                    temp = ''
+                    break
 
-        columns = [column.strip(' ').strip('\n') for column in columns]
+            columns.append(split_line[i])
+            i += 1
 
-        formatted_line = ';'.join(columns) + '\n'
+            if ',' in split_line[i]:
+                i = join_split_line(i, split_line, columns, ',', True)
+            else:
+                columns.append(split_line[i])
+                i += 1
 
-        # replace the current line with the formatted line
-        formatted_lines[formatted_lines.index(line)] = formatted_line
+            if ',' in split_line[i]:
+                i = join_split_line(i, split_line, columns, ',', True)
+            else:
+                columns.append(split_line[i])
+                i += 1
 
-    # add headers
-    formatted_lines.insert(0, 'nr poc;z;odj.;do;przyj.;typ taboru;ilość;termin kursowania\n')
+            temp = ''
+            for j in range(i, len(split_line)):
+                temp += split_line[j] + ' '
+            columns.append(temp.strip())
 
-    with open("output_final.csv", "w", encoding='utf-8') as file:
-        file.writelines(formatted_lines)
+            columns = [column.strip(' ').strip('\n') for column in columns]
+
+            formatted_line = ';'.join(columns) + '\n'
+
+            formatted_lines[formatted_lines.index(line)] = formatted_line
+
+        formatted_lines.insert(0, 'nr poc;z;odj.;do;przyj.;typ taboru;ilość;termin kursowania\n')
+
+        with open("output_final.csv", "w", encoding='utf-8') as file:
+            for line in formatted_lines:
+                file.write(line)
 
 
 format_converted_csv(CSV_PATH)
