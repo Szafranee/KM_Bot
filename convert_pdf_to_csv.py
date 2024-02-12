63690f3659976296797415c180701ad04839edf2
Kacper Szafrański <kacper.szafranski@pjwstk.edu.pl>
2024-02-12 21:03:20 +0100

    Add convert_pdf_to_csv.py

--- convert_pdf_to_csv.py
+++ convert_pdf_to_csv.py
@@ -5,7 +5,6 @@ import pandas as pd
 PDF_PATH = 'data/pdfs/Zestawienie pociągów KM kursujących w dniach 8 II-9 III.pdf'
 CSV_PATH = 'output.csv'
 
-
 # Open the PDF file
 with open(PDF_PATH, 'rb') as file:
     reader = PdfReader(file)
@@ -33,6 +32,46 @@ def replace_second_occurrence(pattern, replacement, string):
     return re.sub(pattern, replacer, string, count=2)
 
 
+def join_split_line(i, split_line, columns, condition, is_until_has):
+    """
+    Processes the split line, joining elements that match the condition
+    until encountering the condition or reaching the end of the line.
+
+    Args:
+        i (int): Starting index for processing.
+        split_line (list): List of split line elements.
+        columns (list): List of columns.
+        condition (str): Condition to check.
+
+    Returns:
+        int: Updated index.
+        :param is_until_has:
+    """
+    # if is_until_has is True, then join until the condition is not found
+    if is_until_has:
+        for j in range(i, len(split_line)):
+            # Check if the element matches the condition or is the last element in the list
+            temp = split_line[j] + ' '
+            while j+1 < len(split_line) and condition in split_line[j]:
+                temp += split_line[j+1] + ' '
+                j += 1
+                i += 1
+            columns.append(temp.strip())
+            break
+        return i + 1
+    else:
+        for j in range(i, len(split_line)):
+            # Check if the element matches the condition or is the last element in the list
+            temp = split_line[j] + ' '
+            while j + 1 < len(split_line) and condition not in split_line[j+1]:
+                temp += split_line[j+1] + ' '
+                j += 1
+                i += 1
+            columns.append(temp.strip())
+            break
+        return i + 1
+
+
 # converted csv formatting
 def format_converted_csv(csv_file):
     with open(csv_file, 'r', encoding='utf-8') as file:
@@ -53,9 +92,6 @@ def format_converted_csv(csv_file):
 
     # removes last 2 lines
     formatted_lines = formatted_lines[:-2]
-
-    # removes empty lines
-    formatted_lines = [line for line in formatted_lines if line.strip()]
 
     # if line ens in "PERON" then add a space after it and add the next line to it
     for i in range(len(formatted_lines)):
@@ -103,7 +139,79 @@ def format_converted_csv(csv_file):
     # replaces SECOND standalone "P" with "Ps"
     formatted_lines = [replace_second_occurrence(r'\bP\b', 'Ps', line) for line in formatted_lines]
 
-    with open("output_2.csv", "w", encoding='utf-8') as file:
+    # removes empty lines
+    formatted_lines = [line for line in formatted_lines if line.strip()]
+
+    for line in formatted_lines:
+        # Split the line into columns
+        columns = []
+        split_line = line.split(' ')
+        i = 0
+        temp = ''
+
+        # add first column
+        columns.append(split_line[i])
+        i += 1
+
+        # add second column
+        for j in range(i, len(split_line)):
+            if not split_line[j].startswith(('0', '1', '2')):
+                temp += split_line[j] + ' '
+                i += 1
+            else:
+                columns.append(temp.strip())
+                temp = ''
+                break
+
+        # add third column
+        columns.append(split_line[i])
+        i += 1
+
+        # add fourth column
+        for j in range(i, len(split_line)):
+            if not split_line[j].startswith(('0', '1', '2')):
+                temp += split_line[j] + ' '
+                i += 1
+            else:
+                columns.append(temp.strip())
+                temp = ''
+                break
+
+        # add fifth column
+        columns.append(split_line[i])
+        i += 1
+
+        # add sixth column
+        if ',' in split_line[i]:
+            i = join_split_line(i, split_line, columns, ',', True)
+        else:
+            columns.append(split_line[i])
+            i += 1
+
+        # add seventh column
+        if ',' in split_line[i]:
+            i = join_split_line(i, split_line, columns, ',', True)
+        else:
+            columns.append(split_line[i])
+            i += 1
+
+        # add seventh column
+        temp = ''
+        for j in range(i, len(split_line)):
+            temp += split_line[j] + ' '
+        columns.append(temp.strip())
+
+        columns = [column.strip(' ').strip('\n') for column in columns]
+
+        formatted_line = ';'.join(columns) + '\n'
+
+        # replace the current line with the formatted line
+        formatted_lines[formatted_lines.index(line)] = formatted_line
+
+    # add headers
+    formatted_lines.insert(0, 'nr poc;z;odj.;do;przyj.;typ taboru;ilość;termin kursowania\n')
+
+    with open("output_final.csv", "w", encoding='utf-8') as file:
         file.writelines(formatted_lines)
 
 
